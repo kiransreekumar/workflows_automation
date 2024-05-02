e51710a066612421fd75ec7f6513e5b54ab8fe85
kiranskmr <105718255+kiranskmr@users.noreply.github.com>
2024-05-03 00:17:32 +0100

    pipeline

---
+++ terraform/modules/azuredevops/wheelfilepipeline.tf
@@ -0,0 +1,41 @@
+
+resource "azuredevops_git_repository" "wheelfilepipeline" {
+  project_id            = azuredevops_project.project.id
+  name       = "workflowsAutomationWheelFile"
+  initialization {
+    init_type   = "Import"
+    source_type = "Git"
+    source_url  = "https://github.com/kiranskmr/workflows_automation/"
+  }
+
+}
+
+
+resource "azuredevops_build_definition" "wheelfilepipelinebuild" {
+  project_id = azuredevops_project.project.id
+  name       = "Wheel file Build Pipeline"
+
+  ci_trigger {
+    use_yaml = true
+  }
+
+  repository {
+    repo_type             = "TfsGit"
+    repo_id               = azuredevops_git_repository.wheelfilepipeline.id 
+    branch_name           = "main"
+    yml_path              = "azure-pipelines-dab-wheel-file.yml"
+  }
+ variable_groups = [azuredevops_variable_group.variable_group.id]
+
+  schedules {
+    branch_filter {
+      include = ["main"]
+      exclude = ["test", "regression"]
+    }
+    days_to_build              = ["Wed", "Sun"]
+    schedule_only_with_changes = true
+    start_hours                = 10
+    start_minutes              = 59
+    time_zone                  = "(UTC) Coordinated Universal Time"
+  }
+}
