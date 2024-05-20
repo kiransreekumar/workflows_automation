0df47cd4739a25438467ed799c6fa582e31658ec
kiranskmr <105718255+kiranskmr@users.noreply.github.com>
2024-05-20 17:07:34 +0100

    remove pipeline var

--- terraform/modules/azuredevops/github.tf
+++ terraform/modules/azuredevops/github.tf
@@ -18,30 +18,30 @@ resource "github_repository_environment" "env" {
   }
 }
 
-resource "github_actions_environment_secret" "env_secret" {
-  repository  = data.github_repository.repo.name
-  environment = github_repository_environment.env.environment
-  secret_name = "SP_TOKEN"
-  plaintext_value = ""
-}
+# resource "github_actions_environment_secret" "env_secret" {
+#   repository  = data.github_repository.repo.name
+#   environment = github_repository_environment.env.environment
+#   secret_name = "SP_TOKEN"
+#   plaintext_value = ""
+# }
 
-resource "github_actions_environment_variable" "node_variable" {
-  repository       = data.github_repository.repo.name
-  environment      = github_repository_environment.env.environment
-  variable_name    = "BUNDLE_VAR_NODE_TYPE"
-  value            = "update this"
-}
+# resource "github_actions_environment_variable" "node_variable" {
+#   repository       = data.github_repository.repo.name
+#   environment      = github_repository_environment.env.environment
+#   variable_name    = "BUNDLE_VAR_NODE_TYPE"
+#   value            = "update this"
+# }
 
-resource "github_actions_environment_variable" "warehouse_variable" {
-  repository       = data.github_repository.repo.name
-  environment      = github_repository_environment.env.environment
-  variable_name    = "BUNDLE_VAR_WAREHOUSE_ID"
-  value            = "update this"
-}
+# resource "github_actions_environment_variable" "warehouse_variable" {
+#   repository       = data.github_repository.repo.name
+#   environment      = github_repository_environment.env.environment
+#   variable_name    = "BUNDLE_VAR_WAREHOUSE_ID"
+#   value            = "update this"
+# }
 
-resource "github_actions_environment_variable" "host_variable" {
-  repository       = data.github_repository.repo.name
-  environment      = github_repository_environment.env.environment
-  variable_name    = "DATABRICKS_HOST"
-  value            = "update this"
-}
+# resource "github_actions_environment_variable" "host_variable" {
+#   repository       = data.github_repository.repo.name
+#   environment      = github_repository_environment.env.environment
+#   variable_name    = "DATABRICKS_HOST"
+#   value            = "update this"
+# }
